10548e6aa3453e5f3ebb0003122db1521b2e03f3
Marvin Kang'ethe <marvin.kangethe@andela.com>
2020-01-21 11:13:46 +0300

    Update lib folder with custom support files

---
+++ lib/custom_logger.rb
@@ -0,0 +1,11 @@
+# Open sourced file
+# lib/custom_logger.rb
+class CustomLogger < Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n"
+  end
+end
+
+logfile = File.open("#{Rails.root}/log/custom.log", 'a')  # create log file
+logfile.sync = true  # automatically flushes data to file
+CUSTOM_LOGGER = CustomLogger.new(logfile)  # constant accessible anywhere
