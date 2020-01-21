14837b625bf69e79bdc05fd9085afa65d83a0083
Marvin Kang'ethe <marvin.kangethe@andela.com>
2020-01-21 10:44:30 +0300

    Add some gems I used most of the time

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,9 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation cannot be found).
+  config.i18n.fallbacks = true
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
